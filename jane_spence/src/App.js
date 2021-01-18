b80920b8fea75a9f7ff61b01752fafa93c32e1be
Jack Beard <jacko.2233@hotmail.com>
2021-01-18 22:53:45 +1100

    Contact Me - HTML & CSS
    Added a contact me form with some basic initial styling. Will need to be made its own page (rather than the homepage) eventually.

--- jane_spence/src/App.js
+++ jane_spence/src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// classes are stateful, functions are not
+class App extends Component{
+  render()
+  {
+    return (
+      <div className="App-header">
+        JANE SPENCE DIABETES EDUCATION
+        <div className="contactMeForm">
+        <h3 className="contactMeTitle">
+			  CONTACT ME
+			  </h3>
+        <h3 className="requiredFields">
+			  * Required Fields
+			  </h3>
+        <form action="store_feedback.php" method="post">
+			  <input name="firstname" type="text" placeholder="* First Name"/>
+		  	<input name="lastname" type="text" placeholder="* Last Name"/>
+			  <input name="email" type="email" placeholder="* Email"/>
+			  <input name="phone" type="number" placeholder="* Phone Number"/>
+			  <input name="date" type="datetime-local"/>
+			  <input name="subject" type="text" placeholder="* Subject"/>
+        <input name="message" type="text" placeholder="* Message"/>
+			  <input className="submit" name="submit" type="submit"/>
+			  </form>
+        </div>
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
