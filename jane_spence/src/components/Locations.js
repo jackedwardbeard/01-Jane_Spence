22d99c8ec88fe89e65e5370204afbc120783349d
Jack Beard <jacko.2233@hotmail.com>
2021-01-19 16:20:20 +1100

    Fixed google maps/navbar/locations page.
    Locations is now a functional component, instead of a class. It now works again with the navbar, and simply embeds the google map using the HTML code.

--- jane_spence/src/components/Locations.js
+++ jane_spence/src/components/Locations.js
@@ -3,21 +3,9 @@ import Navbar from './Navbar/Navbar.js';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './Locations.css';
 
-class Locations extends Component {
+const Locations = () => {
 
 
-    // first thing to call in the constructor(props) is always super(props),
-    // this avoids errors
-    constructor(props) {
-        
-    super(props);
-    
-    this.state = {lat: -38.15773518835747, lng: 145.16931797636528}
-    
-}
-
-
-    render() {
 
    
     return (
@@ -31,37 +19,8 @@ class Locations extends Component {
                         
             </div>
         
-            <Map 
-            className="map"
-            google={this.props.google} 
-            zoom={14}
-            style={{top: '20%', left: '8%', width: '40%', height: '60%', position: 'fixed'}}
-            initialCenter={{ lat: -38.15773518835747, lng: 145.16931797636528}}
-            >
-            <Marker>
-                position={this.state}
-            </Marker>
-            </Map>
-
-            <Map 
-            className="map"
-            google={this.props.google} 
-            zoom={14}
-            style={{top: '20%', left: '50%', width: '40%', height: '60%', position: 'fixed'}}
-            initialCenter={{ lat: -38.15773518835747, lng: 145.16931797636528}}
-            >
-            <Marker>
-                position={this.state}
-            </Marker>
-            </Map>
-
             <div className="mapInformation">
-            <ul id="list">
-                <li>Peninsula Private Hospital</li>
-                <li>525 McClelland Dr, Frankston VIC 3199</li>
-            </ul>
-            </div>
-            <div className="mapInformation">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3137.2465214005506!2d145.16715611532874!3d-38.157709879691545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad60b1b454a6771%3A0xb8da3effaaf10b0b!2sPeninsula%20Private%20Hospital!5e0!3m2!1sen!2sau!4v1611033339679!5m2!1sen!2sau" width="800" height="600" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             <ul id="list">
                 <li>Peninsula Private Hospital</li>
                 <li>525 McClelland Dr, Frankston VIC 3199</li>
@@ -72,7 +31,7 @@ class Locations extends Component {
         </div>
     );
 }
-}
+
 
 
 
