3f5821a2225d57a9cd2c68174a48608202694a0b
Jack Beard <jacko.2233@hotmail.com>
2021-02-21 15:19:53 +1100

    Made navbar background transparent.

--- jane_spence/src/components/Navbar/NavbarComponents.js
+++ jane_spence/src/components/Navbar/NavbarComponents.js
@@ -6,9 +6,9 @@ import { Link as LinkRouter } from 'react-router-dom'
 import { Link as LinkScroll } from 'react-scroll' 
 
 export const Nav = styled.nav`
-    background: black;
+    background: transparent;
     height: 80px;
-    /*margin-top: -80px;*/
+    margin-top: -80px;
     display: flex;
     justify-content: center;
     align-items: center;
