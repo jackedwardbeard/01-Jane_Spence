1f59bb61620a049b970b29d9045020fdf27a1e20
Jack Beard <jacko.2233@hotmail.com>
2021-02-24 18:50:28 +1100

    Added scroll animations to footer.

--- jane_spence/src/components/Footer/index.js
+++ jane_spence/src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useEffect} from 'react'
 import {
     FooterContainer,
     FooterWrapper,
@@ -13,41 +13,48 @@ import {
     NavLink,
     NavLinkScroll
 } from './FooterComponents'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <FooterContainer>
             <FooterWrapper>
                 <Row1>
                     <Column1>
-                        <Title>
+                        <Title data-aos='fade-down'>
                             PAGES
                         </Title>
-                        <NavLinkScroll to="landing" smooth={true} spy={true} offset={-80}>Home</NavLinkScroll>
-                        <NavLinkScroll to="about" smooth={true} spy={true} offset={-80}>About</NavLinkScroll>
-                        <NavLinkScroll to="faq" smooth={true} spy={true} offset={-80}>FAQ</NavLinkScroll>
-                        <NavLinkScroll to="location" smooth={true} spy={true} offset={-80}>Location</NavLinkScroll>
-                        <NavLinkScroll to="contact" smooth={true} spy={true} offset={-80}>Contact</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="landing" smooth={true} spy={true} offset={-80}>Home</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-right' to="about" smooth={true} spy={true} offset={-80}>About</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="faq" smooth={true} spy={true} offset={-80}>FAQ</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-right' to="location" smooth={true} spy={true} offset={-80}>Location</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="contact" smooth={true} spy={true} offset={-80}>Contact</NavLinkScroll>
                     </Column1>
                     <Column2>
-                        <Title>
+                        <Title data-aos='fade-down'>
                             IMPORTANT WEBSITES
                         </Title>
-                        <NavLink to="//google.com" target="_blank">Website 1</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 2</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 3</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 4</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 5</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 1</NavLink>
+                        <NavLink data-aos='fade-right' to="//google.com" target="_blank">Website 2</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 3</NavLink>
+                        <NavLink data-aos='fade-right' to="//google.com" target="_blank">Website 4</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 5</NavLink>
                     </Column2>
                     <Column3>
-                        <Title>
+                        <Title data-aos='fade-down'>
                             OTHER
                         </Title>
-                        <NavLink to="//google.com" target="_blank">Website 6</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 7</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 8</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 9</NavLink>
-                        <NavLink to="//google.com" target="_blank">Website 10</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 6</NavLink>
+                        <NavLink data-aos='fade-right' to="//google.com" target="_blank">Website 7</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 8</NavLink>
+                        <NavLink data-aos='fade-right' to="//google.com" target="_blank">Website 9</NavLink>
+                        <NavLink data-aos='fade-left' to="//google.com" target="_blank">Website 10</NavLink>
                     </Column3>
                 </Row1>
             </FooterWrapper>
