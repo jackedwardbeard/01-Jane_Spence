84f5b11b183f89a60da62f215dbf50dc3d84859f
Jack Beard <jacko.2233@hotmail.com>
2021-03-04 01:12:31 +1100

    Landing page - removing obsolete media queries.

--- jane_spence/src/components/Landing/LandingComponents.js
+++ jane_spence/src/components/Landing/LandingComponents.js
@@ -13,58 +13,6 @@ export const LandingContainer = styled.div`
     height: 100vh;
     background: black;
     overflow: hidden;
-
-    @media screen and (max-width: 768px) {
-        width: 100%;
-        overflow-x: hidden;
-
-    }
-
-    @media screen and (max-width: 1024px) and (max-height: 1366px) {
-        height: 1366px;
-    }
-
-    @media screen and (max-width: 768px) and (max-height: 1024px) {
-        height: 1024px;
-    }
-
-    @media screen and (max-width: 540px) and (max-height: 720px) {
-        height: 720px;
-    }
-
-    @media screen and (max-width: 414px) and (max-height: 736px) {
-        height: 736px;
-    }
-
-    @media screen and (max-width: 411px) and (max-height: 823px) {
-        height: 823px;
-    }
-
-    @media screen and (max-width: 411px) and (max-height: 731px) {
-        height: 731px;
-    }
-
-    @media screen and (max-width: 375px) and (max-height: 812px) {
-        height: 812px;
-    }
-
-    @media screen and (max-width: 375px) and (max-height: 667px) {
-        height: 667px;
-    }
-
-    @media screen and (max-width: 360px) and (max-height: 640px) {
-        height: 640px;
-    }
-
-    @media screen and (max-width: 320px) and (max-height: 568px) {
-        height: 568px;
-    }
-
-    @media screen and (max-width: 280px) and (max-height: 653px) {
-        height: 653px;
-    }
-    
-
 `
 
 export const LandingBackground = styled.div`
@@ -215,26 +163,6 @@ export const LearnMoreBtn = styled(LinkScroll)`
     text-decoration: none;
     z-index: 999;
     margin-top: 10px;
-
-    @media screen and (max-width: 1440px) {
-        font-size: 0.9rem;
-    }
-
-    @media screen and (max-width: 540px) {
-        font-size: 1rem;
-    }
-
-    @media screen and (max-width: 360px) {
-        font-size: 0.9rem;
-    }
-
-    @media screen and (max-width: 320px) {
-        font-size: 0.8rem;
-    }
-
-    @media screen and (max-width: 280px) {
-        font-size: 0.65rem;
-    }
 
     &:hover {
         transition: all 0.2s ease-in-out;
