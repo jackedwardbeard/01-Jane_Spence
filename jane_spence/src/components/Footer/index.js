9e54970abe9996ad5071a1fed70e2f268937ddd7
Jack Beard <jacko.2233@hotmail.com>
2021-03-06 00:24:29 +1100

    Edited footer scroll animations.

--- jane_spence/src/components/Footer/index.js
+++ jane_spence/src/components/Footer/index.js
@@ -31,33 +31,33 @@ const Footer = () => {
                     For further information, visit the websites listed below or book a consultation @ 0417 389 257.
                 </PreFooterWrapper>
                 <Row1>
-                    <Column1 data-aos='fade-up-right'>
-                        <Title>
+                    <Column1>
+                        <Title data-aos='fade-right'>
                             PAGES
                         </Title>
-                        <NavLinkScroll to="landing" smooth={true} spy={true} offset={-80}>Home</NavLinkScroll>
-                        <NavLinkScroll to="about" smooth={true} spy={true} offset={-80}>About</NavLinkScroll>
-                        <NavLinkScroll to="faq" smooth={true} spy={true} offset={-80}>FAQ</NavLinkScroll>
-                        <NavLinkScroll to="locations" smooth={true} spy={true} offset={-80}>Locations</NavLinkScroll>
-                        <NavLinkScroll to="contact" smooth={true} spy={true} offset={-30}>Contact</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="landing" smooth={true} spy={true} offset={-80}>Home</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-right' to="about" smooth={true} spy={true} offset={-80}>About</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="faq" smooth={true} spy={true} offset={-80}>FAQ</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-right' to="locations" smooth={true} spy={true} offset={-80}>Locations</NavLinkScroll>
+                        <NavLinkScroll data-aos='fade-left' to="contact" smooth={true} spy={true} offset={-30}>Contact</NavLinkScroll>
                     </Column1>
-                    <Column2 data-aos='fade-up'>
-                        <Title>
+                    <Column2>
+                        <Title data-aos='fade-right'>
                             HELPFUL WEBSITES
                         </Title>
-                        <NavLink to="//diabetesaustralia.com.au" target="_blank">Diabetes Australia</NavLink>
-                        <NavLink to="//ndss.com.au" target="_blank">NDSS</NavLink>
-                        <NavLink to="//baker.edu.au/health-hub/fact-sheets" target="_blank">Baker Institute</NavLink>
+                        <NavLink data-aos='fade-left' to="//diabetesaustralia.com.au" target="_blank">Diabetes Australia</NavLink>
+                        <NavLink data-aos='fade-right' to="//ndss.com.au" target="_blank">NDSS</NavLink>
+                        <NavLink data-aos='fade-left' to="//baker.edu.au/health-hub/fact-sheets" target="_blank">Baker Institute</NavLink>
                     </Column2>
-                    <Column3 data-aos='fade-up-left'>
-                        <Title>
+                    <Column3>
+                        <Title data-aos='fade-right'>
                             TOPICS
                         </Title>
-                        <NavLink to="//diabetesaustralia.com.au/about-diabetes/what-is-diabetes/" target="_blank">What Is Diabetes?</NavLink>
-                        <NavLink to="//ndss.com.au/about-diabetes/newly-diagnosed/" target="_blank">Newly Diagnosed?</NavLink>
-                        <NavLink to="//ndss.com.au/living-with-diabetes/managing-diabetes/blood-glucose-monitoring/" target="_blank">Blood Glucose Monitoring</NavLink>
-                        <NavLink to="//baker.edu.au/-/media/documents/fact-sheets/baker-institute-factsheet-carbohydrates-and-glycaemic-index.pdf" target="_blank">Low GI Carbohydrates</NavLink>
-                        <NavLink to="//baker.edu.au/-/media/documents/fact-sheets/baker-institute-supermarket-shopping-guide.pdf" target="_blank">Supermarket Shopping Guide</NavLink>
+                        <NavLink data-aos='fade-left' to="//diabetesaustralia.com.au/about-diabetes/what-is-diabetes/" target="_blank">What Is Diabetes?</NavLink>
+                        <NavLink data-aos='fade-right' to="//ndss.com.au/about-diabetes/newly-diagnosed/" target="_blank">Newly Diagnosed?</NavLink>
+                        <NavLink data-aos='fade-left' to="//ndss.com.au/living-with-diabetes/managing-diabetes/blood-glucose-monitoring/" target="_blank">Blood Glucose Monitoring</NavLink>
+                        <NavLink data-aos='fade-right' to="//baker.edu.au/-/media/documents/fact-sheets/baker-institute-factsheet-carbohydrates-and-glycaemic-index.pdf" target="_blank">Low GI Carbohydrates</NavLink>
+                        <NavLink data-aos='fade-left' to="//baker.edu.au/-/media/documents/fact-sheets/baker-institute-supermarket-shopping-guide.pdf" target="_blank">Supermarket Shopping Guide</NavLink>
                     </Column3>
                 </Row1>
             </FooterWrapper>
