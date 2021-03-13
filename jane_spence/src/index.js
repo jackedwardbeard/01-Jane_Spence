23d5649c2bcd530e58a76ac0db6adfe2d1af48bd
Jack Beard <jacko.2233@hotmail.com>
2021-03-13 15:13:23 +1100

    Added polyfill to package.json.

--- jane_spence/src/index.js
+++ jane_spence/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 ReactDOM.render(
