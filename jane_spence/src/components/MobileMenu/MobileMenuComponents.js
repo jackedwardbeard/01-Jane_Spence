52023234158bd25e6597e6445ea69a041929f528
Jack Beard <jacko.2233@hotmail.com>
2021-09-06 19:07:41 +1000

    Small typo fix

--- jane_spence/src/components/MobileMenu/MobileMenuComponents.js
+++ jane_spence/src/components/MobileMenu/MobileMenuComponents.js
@@ -16,7 +16,7 @@ export const MobileMenuContainer = styled.aside`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center:
+    align-items: center;
     top: 0;
     left: 0;
     transition: 0.3s ease-in-out;
